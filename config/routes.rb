86a12f25ed0278e8784df82364976c12245ea7f4
Nam <nambn1996@gmail.com>
2018-10-10 03:58:49 +0700

    Authenticate user using devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, default: { format: :json } do
+	  mount_devise_token_auth_for 'User', at: 'users'
+	  resources :users, only: [:index, :show]
   end
 end
