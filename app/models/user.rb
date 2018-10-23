ca96c52ddbfb816cc564ada4c63ecd84ff284949
H2V <h2v9696@gmail.com>
2018-10-23 17:31:09 +0700

    Change user's default image to cloudinary image

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,10 @@ class User < ApplicationRecord
 
   def add_missing_fields
   	if self.image_url.blank?
-    	self.image_url = Faker::Avatar.unique.image
+    	self.image_url = "default_avar.png"
     end
     if self.cover_url.blank?
-    	self.cover_url = "https://placeholdit.imgix.net/~text?txtsize=80&txt=800x300&w=800&h=300"
+    	self.cover_url = "banner_default.png"
     end
   end
 
