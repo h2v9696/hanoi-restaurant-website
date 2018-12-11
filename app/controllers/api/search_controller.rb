2542648beb972a7bf760c77c10efe0f0ea27eca0
Nam <nambn1996@gmail.com>
2018-12-11 17:41:16 +0700

    search filter

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,11 +1,42 @@
 class Api::SearchController < ApplicationController
   def index
-    render json: {
-      status: :success,
-      data: Restaurant.where(
-        'name LIKE :q OR address LIKE :q OR description LIKE :q',
-        q: "%#{params[:q]}%"
-      ).as_json(methods: [:rating_avg, :rating_count])
-    }
+    if params[:type] == "0" || !params[:type].present?
+      render json: {
+        status: :success,
+        data: Restaurant.where(
+          'name LIKE :q OR address LIKE :q OR description LIKE :q',
+          q: "%#{params[:q]}%"
+        ).as_json(methods: [:rating_avg, :rating_count])
+      }
+    elsif params[:type] == "1"
+      render json: {
+        status: :success,
+        data: Restaurant.where(
+          'name LIKE :q',
+          q: "%#{params[:q]}%"
+        ).as_json(methods: [:rating_avg, :rating_count])
+      }
+    elsif params[:type] == "2"
+      render json: {
+        status: :success,
+        data: Restaurant.where(
+          'address LIKE :q',
+          q: "%#{params[:q]}%"
+        ).as_json(methods: [:rating_avg, :rating_count])
+      }
+    elsif params[:type] == "3"
+      render json: {
+        status: :success,
+        data: Restaurant.where(
+          'description LIKE :q',
+          q: "%#{params[:q]}%"
+        ).as_json(methods: [:rating_avg, :rating_count])
+      }
+    else
+      render json: {
+        status: :success,
+        data: []
+      }
+    end
   end
 end
